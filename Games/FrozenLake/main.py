2d8a78ff81b3ff3a8e57e2fafa0a803589983a22
Luozan <550347322@qq.com>
2025-11-03 11:14:45 +0800

    Update main.py

--- Games/FrozenLake/main.py
+++ Games/FrozenLake/main.py
@@ -4,7 +4,7 @@ import random as rd
 from tqdm import trange
 
 env = gym.make("FrozenLake-v1", map_name="4x4",
-               is_slippery=False, render_mode="rgb_array")
+               is_slippery=True, render_mode="rgb_array")
 print("_____OBSERVATION SPACE_____ \n")
 print("Observation Space", env.observation_space)
 print("Sample observation", env.observation_space.sample())
@@ -35,10 +35,6 @@ def epsilon_greedy_policy(Qtable, state, epsilon):
     return action
 
 
-learning_rate = 0.7
-gamma = 0.99
-
-
 def train(episodes, min_epsilon, max_epsilon, decay_rate, env, max_steps, Qtable):
     for episode in trange(episodes):
         epsilon = min_epsilon + \
@@ -53,11 +49,59 @@ def train(episodes, min_epsilon, max_epsilon, decay_rate, env, max_steps, Qtable
 
             Qtable[state][action] += learning_rate * \
                 (reward + gamma *
-                np.max(Qtable[new_state]) - Qtable[state][action])
-            
+                 np.max(Qtable[new_state]) - Qtable[state][action])
+
             if terminated or truncated:
                 break
-            
+
             state = new_state
     return Qtable
+
+
+def evaluate_agent(env, max_steps, n_eval_episodes, Q, seed):
+    """
+    Evaluate the agent for ``n_eval_episodes`` episodes and returns average reward and std of reward.
+    :param env: The evaluation environment
+    :param n_eval_episodes: Number of episode to evaluate the agent
+    :param Q: The Q-table
+    :param seed: The evaluation seed array (for taxi-v3)
+    """
+    episode_rewards = []
+    for episode in trange(n_eval_episodes):
+        if seed:
+            state, info = env.reset(seed=seed[episode])
+        else:
+            state, info = env.reset()
+        step = 0
+        truncated = False
+        terminated = False
+        total_rewards_ep = 0
+
+        for step in range(max_steps):
+            # Take the action (index) that have the maximum expected future reward given that state
+            action = greedy_policy(Q, state)
+            new_state, reward, terminated, truncated, info = env.step(action)
+            total_rewards_ep += reward
+
+            if terminated or truncated:
+                break
+            state = new_state
+        episode_rewards.append(total_rewards_ep)
+    mean_reward = np.mean(episode_rewards)
+    std_reward = np.std(episode_rewards)
+
+    return mean_reward, std_reward
         
+
+
+learning_rate = 0.7
+gamma = 0.99
+max_steps = 99
+n_eval_episodes = 10000
+eval_seed = []
+
+Qtable_frozenlake = train(n_eval_episodes, 0.05, 1, 0.0005, env, max_steps, Qtable_frozenlake)
+
+# Evaluate our Agent
+mean_reward, std_reward = evaluate_agent(env, max_steps, n_eval_episodes, Qtable_frozenlake, eval_seed)
+print(f"Mean_reward={mean_reward:.2f} +/- {std_reward:.2f}")
